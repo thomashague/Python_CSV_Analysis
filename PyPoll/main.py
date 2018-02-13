ea494b41e21df529c3186e2920e62dbcc5274f12
Thomas Hague <thomascrocketthague@gmail.com>
2018-02-13 12:57:20 -0800

    final1

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,9 +2,10 @@ import os
 import csv
 import random
 
-#NOTE: change this path depending on what data file you want to use
-csvPath = input('What is the path of the input file you want to use? ex: election_data_1.csv')
+#get path of file
+csvPath = input('What is the path of the input file you want to use? ex: election_data_1.csv ')
 
+#set up variables
 rowCount = 0
 candidate = 'Bob'
 candidateArray = []
@@ -16,22 +17,26 @@ Winner = 'Bob'
 mostSoFar = 0
 candidateCounter = 0
 
+#open data file
 with open(csvPath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     next(csvreader, None) #skip the headers
 
+    #get total votes, candidates
     for row in csvreader:
         rowCount = rowCount + 1
         candidate = str(row[2])
         if(candidate in candidateDictionary):
-            candidateDictionary[candidate][0] = candidateDictionary[candidate][0] + 1 #do something to satisfy the computer...
+            candidateDictionary[candidate][0] = candidateDictionary[candidate][0] + 1 #increase vote count for candidate
         else:
-            candidateDictionary[candidate] = [0,0]
+            candidateDictionary[candidate] = [0,0] 
             candidateDictionary[candidate][0] = 1 #add the candidate to the array
 
+    #get percentage of votes candidate won
     for x in candidateDictionary:
         candidateDictionary[x][1] = ( ((candidateDictionary[x][0])/rowCount)*100)
 
+    #find winner
     for x in candidateDictionary:
         if(candidateDictionary[x][1] > mostSoFar):
             mostSoFar = candidateDictionary[x][1]
@@ -41,12 +46,7 @@ with open(csvPath, newline="") as csvfile:
             #do nothing
 
 
-        
-print(rowCount)
-print(candidateDictionary)
-print(winner)
-
-
+#set up print statements
 line1 = "Election Results"
 line2 = "-------------------------"
 line3 = "Total Votes: " + str(rowCount)
@@ -69,6 +69,8 @@ print(line5)
 print(line6)
 print(line7)
 
+
+#export to text file within current directory
 outputPath = 'theresNoWayYouHaveaFileWithThisName' + str(random.randint(0,1000)) + '.txt'
 f = open(outputPath,'w')
 f.write(line1 + "\n")
